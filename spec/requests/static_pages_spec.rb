debc78a184cc5652f6efdbe4936719426a03c1f7
Tara Krishnan <tara.krish16@gmail.com>
2012-06-06 10:04:17 -0400

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,23 +6,29 @@ describe "StaticPages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sampe App'" do
+    it "should have the h1 'Sampe App'" do
     	visit '/static_pages/home'
     	page.should have_selector('h1', 
     		:text => "Sample App")
 
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
     	visit '/static_pages/home'
     	page.should have_selector('title',
-    		:text =>"#{base_title} | Home")
+    		:text =>"#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+    	visit '/static_pages/home'
+    	page.should_not have_selector('title',
+    		:text => " | Home")
     end
   end
 
   describe "Help page" do
 
-  	it "should have the content 'Help'" do
+  	it "should have the h1 'Help'" do
   		visit '/static_pages/help'
   		page.should have_selector('h1',
   			:text => "Help")
@@ -37,7 +43,7 @@ describe "StaticPages" do
 
   describe "About page" do
 
-  	it "should have the content 'About Us'" do
+  	it "should have the h1 'About Us'" do
   		visit '/static_pages/about'
   		page.should have_selector('h1',
   			:text => "About Us")
@@ -51,7 +57,7 @@ describe "StaticPages" do
 
   describe "Contact page" do
 
-  	it "should have the content 'Contact'" do
+  	it "should have the h1 'Contact'" do
   		visit '/static_pages/contact'
   		page.should have_selector('h1',
   			:text => "Contact")
